f15dab194b43d32701bd8a2e6264f9133e6d9ed9
Jordi Adell <jadell@verbio.com>
2016-03-23 18:44:25 +0100

    try/catch for gtest exceptions

--- test/test_wipp.cpp
+++ test/test_wipp.cpp
@@ -335,6 +335,13 @@ TEST(randTest, init_delete)
 
 int main(int argc, char* argv[])
 {
+  try
+  {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+  }
+  catch (...)
+  {
+    std::cerr << "WIPP tests failed" << std::endl;
+  }
 }
