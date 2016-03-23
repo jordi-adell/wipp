dfa1b3ed8db62d01793430b186337435ba3fe0cb
Jordi Adell <jadell@verbio.com>
2016-03-23 18:43:26 +0100

    circular buffer unit test

--- test/test_wipp.cpp
+++ test/test_wipp.cpp
@@ -159,6 +159,117 @@ TEST(circularBufferTest, init_delete)
 
 
 
+TEST(circularBufferTest, write_read)
+{
+
+  wipp::wipp_circular_buffer_t *cf;
+  const size_t cf_length = 1024;
+  const size_t step = 131;
+  size_t stored;
+  size_t occupancy = 0;
+  size_t free;
+  size_t data_position = 0;
+  size_t output_position = 0;
+  wipp::init_cirular_buffer<double>(&cf, cf_length);
+  double data[2*cf_length];
+  double output[2*cf_length];
+
+  wipp::cf_read(cf, output, step, &stored);
+
+  EXPECT_TRUE(stored == 0);
+
+  for (size_t i = 0; i < 2*cf_length; ++i)
+    data[i] = 0;
+
+  wipp::cf_free(cf, &free);
+
+  for (data_position = 0; data_position < 2*cf_length && free > 0; )
+  {
+    wipp::cf_free(cf, &free);
+    wipp::cf_write(cf,&data[data_position], step, &stored);
+    EXPECT_TRUE(stored == std::min(step, free));
+    if (data_position < cf_length - step)
+    {
+      EXPECT_EQ(stored, step);
+    }
+
+    data_position += stored;
+
+    size_t new_occ = occupancy + stored;
+    wipp::cf_occupancy(cf, &occupancy);
+    EXPECT_EQ(occupancy, new_occ);
+
+    DEBUG_STREAM("Free: " << free << " stored: " << stored);
+
+    if (stored == 0)
+    {
+      EXPECT_EQ(data_position, cf_length);
+    }
+    else if (stored < step)
+    {
+      size_t remaining = cf_length%step;
+      EXPECT_EQ(stored, remaining);
+    }
+  }
+
+  wipp::cf_occupancy(cf, &occupancy);
+
+  EXPECT_EQ(occupancy, cf_length);
+  EXPECT_EQ(data_position, cf_length);
+
+  for (stored = 0; data_position < 2*cf_length; )
+  {
+    size_t read_data;
+    wipp::cf_read(cf, &output[output_position], step, &read_data);
+    output_position += read_data;
+
+    EXPECT_EQ(read_data, step);
+
+    size_t new_occ = occupancy - read_data;
+    wipp::cf_occupancy(cf, &occupancy);
+    EXPECT_EQ(occupancy, new_occ);
+
+    if (step <  2*cf_length - data_position)
+    {
+      wipp::cf_write(cf, &data[data_position], step, &stored);
+      EXPECT_EQ(stored, step);
+      EXPECT_EQ(stored, read_data);
+    }
+    else
+    {
+      wipp::cf_write(cf, &data[data_position], 2*cf_length - data_position, &stored);
+      EXPECT_EQ(stored, 2*cf_length - data_position);
+    }
+
+    data_position += stored;
+
+    new_occ = occupancy + stored;
+    wipp::cf_occupancy(cf, &occupancy);
+    EXPECT_EQ(occupancy, new_occ);
+  }
+
+  EXPECT_EQ(data_position, 2*cf_length);
+  EXPECT_EQ(occupancy, cf_length - step + stored);
+
+  do
+  {
+    size_t remaining_space = 2*cf_length - output_position;
+    if (remaining_space < step)
+      wipp::cf_read(cf, &output[output_position], remaining_space, &stored);
+    else
+      wipp::cf_read(cf, &output[output_position], step, &stored);
+
+    output_position += stored;
+  } while (stored > 0);
+
+  wipp::cf_occupancy(cf, &occupancy);
+  EXPECT_EQ(occupancy, 0);
+
+  EXPECT_EQ(output_position, 2*cf_length);
+
+}
+
+
 TEST(fftTest, init_delete)
 {
 
